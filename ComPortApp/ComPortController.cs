133a433696164b867509e231d29a7aa49d3702f9
Mikhail Solntsev <mik-solncev@ya.ru>
2013-04-13 20:27:18 +0300

    Fixing recursion bug

--- ComPortApp/ComPortController.cs
+++ ComPortApp/ComPortController.cs
@@ -52,8 +52,8 @@ namespace ComPortApp
         
         private void InitPortListerning()
         {
-            InitKeyCommandsHandling();
             _port.DataReceived += _port_DataReceived;
+            InitKeyCommandsHandling();
         }
 
         private void _port_DataReceived(object sender, SerialDataReceivedEventArgs e)
@@ -145,7 +145,7 @@ namespace ComPortApp
         }
 
         private void SendBytesToPort(byte[] bytesToSend)
-        {
+        {/
             _port.Write(bytesToSend, 0, 3);
             LogPortDataSending(bytesToSend);
         }
